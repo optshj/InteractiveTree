8ece8b0b6307771b9763b17b9f0d0faefdedcd20
optshj <optshj@gmail.com>
2023-10-19 14:32:10 +0000

    [Feat] 나무그리기 구현

--- src/component/Canvas.tsx
+++ src/component/Canvas.tsx
@@ -5,7 +5,8 @@ function Canvas(){
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const contextRef = useRef(null);
 	const [ctx,setCtx] = useState();
-	
+	const size:number = 11; //나무의 전체적인 크기
+	const frame = 15; //나무가 생성되는 속도
 	useEffect(()=> {
 		const canvas = canvasRef.current;
 		canvas.width = window.innerWidth;
@@ -15,15 +16,37 @@ function Canvas(){
 		contextRef.current = context;
 		setCtx(context);
 	},[]);
+	function degToRad(angle) {
+		return (angle / 180.0) * Math.PI;
+	}
+	function cos(angle) {
+    	return Math.cos(degToRad(angle));
+	}
+	function sin(angle) {
+		return Math.sin(degToRad(angle));
+	}
+	function random(min,max){
+		return min + Math.floor(Math.random() * (max - min + 1));
+	}
+	function branch(startX:number,startY:number,endX:number,endY:number,depth:number,width:number,angle:number){
+		if (size === depth) return;
+		
+		ctx.beginPath();
+		ctx.lineWidth = width;
+		ctx.moveTo(startX,startY);
+		ctx.lineTo(endX,endY);
+		ctx.stroke();
+		const len = depth == 0 ? random(7,10):random(0,12);
+		const nextX:number = endX + cos(angle)*(size-depth)*len;
+		const nextY:number = endY + sin(angle)*(size-depth)*len;
+		requestAnimationFramebranch((endX,endY,nextX,nextY,depth+1,width-1,angle+random(12,23)));
+		requestAnimationFramebranch((endX,endY,nextX,nextY,depth+1,width-1,angle-random(12,23)));
+		
+	}
 	const drawing = e => {
 		const offsetX = e.nativeEvent.offsetX;
 		const offsetY = e.nativeEvent.offsetY;
-		const len = 200;
-		ctx.lineWidth = 10;
-		ctx.moveTo(offsetX,0);
-		ctx.lineTo(offsetX,len);
-		ctx.stroke();
-		
+		requestAnimationFrame(branch(offsetX,window.innerHeight,offsetX,window.innerHeight,0,size,-90));
 	}
 	return(
 		<div className={styles.Canvas}>
