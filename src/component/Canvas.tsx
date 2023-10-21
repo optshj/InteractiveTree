2da7017221269378fb37317fd524eea781438770
optshj <optshj@gmail.com>
2023-10-21 14:51:41 +0000

    [Feat] 나무 생성 애니메이션 추가

--- src/component/Canvas.tsx
+++ src/component/Canvas.tsx
@@ -1,12 +1,12 @@
-import {useRef,useState,useEffect,useCallback } from 'react';
-import styles from '../css/Canvas.module.css';
+import {useRef,useState,useEffect } from 'react';
 
 function Canvas(){
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const contextRef = useRef(null);
 	const [ctx,setCtx] = useState();
 	const size:number = 11; //나무의 전체적인 크기
-	const frame = 15; //나무가 생성되는 속도
+	const frame = 60; //나무가 생성되는 속도
+	
 	useEffect(()=> {
 		const canvas = canvasRef.current;
 		canvas.width = window.innerWidth;
@@ -25,31 +25,46 @@ function Canvas(){
 	function sin(angle) {
 		return Math.sin(degToRad(angle));
 	}
-	function random(min,max){
+	function random(min,max){/*min,max 사이의 랜덤한 수 리턴*/
 		return min + Math.floor(Math.random() * (max - min + 1));
 	}
-	function branch(startX:number,startY:number,endX:number,endY:number,depth:number,width:number,angle:number){
-		if (size === depth) return;
-		
+	function draw(startX,startY,gapX,gapY,currentframe,width){
+		if (currentframe == frame){
+			cancelAnimationFrame(draw);
+			return;
+		}
+		requestAnimationFrame(()=>draw(startX+gapX,startY+gapY,gapX,gapY,currentframe+1,width));
+		ctx.beginPath();
+		ctx.lineWidth = width;
+		ctx.moveTo(startX,startY);
+		ctx.lineTo(startX+gapX,startY+gapY);
+		ctx.stroke();
+	}
+	function branch(startX,startY,endX,endY,depth,width,angle,currentFrame){
+		if (size === depth) {
+			cancelAnimationFrame(branch);
+			return;
+		};
 		ctx.beginPath();
 		ctx.lineWidth = width;
 		ctx.moveTo(startX,startY);
 		ctx.lineTo(endX,endY);
 		ctx.stroke();
+		const gapX:number = (endX-startX)/frame;
+		const gapY:number = (endY-startY)/frame;
 		const len = depth == 0 ? random(7,10):random(0,12);
 		const nextX:number = endX + cos(angle)*(size-depth)*len;
 		const nextY:number = endY + sin(angle)*(size-depth)*len;
-		requestAnimationFramebranch((endX,endY,nextX,nextY,depth+1,width-1,angle+random(12,23)));
-		requestAnimationFramebranch((endX,endY,nextX,nextY,depth+1,width-1,angle-random(12,23)));
-		
+		requestAnimationFrame(()=>branch(endX,endY,nextX,nextY,depth+1,width-1,angle+random(12,23),0));
+		requestAnimationFrame(()=>branch(endX,endY,nextX,nextY,depth+1,width-1,angle-random(12,23),0));
 	}
 	const drawing = e => {
 		const offsetX = e.nativeEvent.offsetX;
-		const offsetY = e.nativeEvent.offsetY;
-		requestAnimationFrame(branch(offsetX,window.innerHeight,offsetX,window.innerHeight,0,size,-90));
+		
+		requestAnimationFrame(()=>branch(offsetX,window.innerHeight,offsetX,window.innerHeight,0,size,-90));
 	}
 	return(
-		<div className={styles.Canvas}>
+		<div>
 			<canvas ref={canvasRef} onMouseDown={e =>drawing(e)}></canvas>
 		</div>
 	)
